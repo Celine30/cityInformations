19fa80ae06a4a9e87bde67d9dfd28463d7574d01
celine CAURIER <celi.caurier@gmail.com>
2020-11-03 12:27:18 +0100

    inscription

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("ok");
+
+let inscription = false
+
+document.getElementById('boutonInscription').onclick= function(event){
+    event.preventDefault();
+    if(inscription == false) {
+        document.getElementById('formInscription').classList.remove("noVisible");
+        inscription = true;
+    }else{
+        document.getElementById('formInscription').classList.add("noVisible");
+        inscription = false;
+
+    }
+    
+
+}
+
+
